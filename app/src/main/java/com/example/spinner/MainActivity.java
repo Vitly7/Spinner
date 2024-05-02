408e695bb67b0155af0e592a8394cb8811dc2df2
Vitly7 <111552374+Vitly7@users.noreply.github.com>
2024-05-02 12:09:59 +0700

    d

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.spinner;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -14,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner _spinner1, _spinner2, _spinner3, _spinner4;
+    private Spinner _spinner1, _spinner2, _spinner3, _spinner4, _spinner5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +28,13 @@ public class MainActivity extends AppCompatActivity {
         _spinner2 = (Spinner) findViewById(R.id.spinner2);
         _spinner3 = (Spinner) findViewById(R.id.spinner3);
         _spinner4 = (Spinner) findViewById(R.id.spinner4);
+        _spinner5 = (Spinner) findViewById(R.id.spinner5);
 
         initSpinner1();
         initSpinner2();
         initSpinner3();
         initSpinner4();
+        initSpinner5();
     }
 
     private void initSpinner1() {
@@ -77,6 +82,46 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter4 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, genapList);
         _spinner4.setAdapter(arrayAdapter4);
     }
+
+    private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        int i = 5;
+        while (i * i <= n) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+            i += 6;
+        }
+        return true;
+    }
+
+    private void initSpinner5() {
+        List<String> primeList = new ArrayList<>();
+
+        for (int i = 1; i <= 1000; i++) {
+            if (isPrime(i)) {
+                primeList.add("Bilangan Prima ke-" + i);
+            }
+        }
+
+        ArrayAdapter<String> arrayAdapter5 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, primeList);
+        _spinner5.setAdapter(arrayAdapter5);
+    }
+
+
+
+
+
+
+
 
 
 }
